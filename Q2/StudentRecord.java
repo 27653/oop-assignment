2b6cbeb787c7940cf391936ab7a25e879ebe97ec
27653 <pichryniyirema@gmail.com>
2025-11-22 23:22:43 +0200

    Add files via upload

---
+++ Q2/StudentRecord.java
@@ -0,0 +1,9 @@
+package _27653.Q2;
+
+public final class StudentRecord extends Fee {
+
+    public double calculateAverageMarks() {
+        if (getMaxMarks() == 0) return 0.0; // Prevent division by zero
+        return ((double) getObtainedMarks() / getMaxMarks()) * 100.0;
+    }
+}
---
+++ Q3/EmployeeDataException.java
@@ -0,0 +1,7 @@
+package _27653.Q3;
+
+public class EmployeeDataException extends RuntimeException {
+    public EmployeeDataException(String message) {
+        super(message);
+    }
+}
